f56f206597c771f7b9df3fbec6abbf33d800d1fd
Minsoo Kang <alstn38@naver.com>
2025-04-24 17:46:14 +0900

    feat: HTTPMethod Model 정의

---
+++ AppStalk/AppStalk/Network/Common/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/24/25.
+//
+
+import Foundation
+
+/// HTTP 메소드 열거형
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
