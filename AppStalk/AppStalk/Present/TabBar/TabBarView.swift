73fb8faf109de88c45a8baeeb33f3d3aa6c2b66d
Minsoo Kang <alstn38@naver.com>
2025-04-24 19:17:12 +0900

    feat: TabBarView 구현

---
+++ AppStalk/AppStalk/Present/TabBar/TabBarView.swift
@@ -0,0 +1,47 @@
+//
+//  TabBarView.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/24/25.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    var body: some View {
+        TabView {
+            TodayView()
+                .tabItem {
+                    Image(systemName: "text.rectangle.page")
+                    Text("투데이")
+                }
+
+            GameView()
+                .tabItem {
+                    Image(systemName: "gamecontroller")
+                    Text("게임")
+                }
+                .tabItem { Image(systemName: "gamecontroller") }
+            
+            UserAppView()
+                .tabItem {
+                    Image(systemName: "square.stack.3d.up.fill")
+                    Text("앱")
+                }
+            
+            ArcadeView()
+                .tabItem {
+                    Image(systemName: "arcade.stick")
+                    Text("Arcade")
+                }
+            
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("검색")
+                }
+        }
+        .tint(.accentColor)
+    }
+}
