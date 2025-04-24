7ce54a3a9e37835ef2352ecb670a386db1a9c810
Minsoo Kang <alstn38@naver.com>
2025-04-24 15:22:03 +0900

    init: AppStalk project 생성

---
+++ AppStalk/AppStalk/App/AppStalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStalkApp.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
