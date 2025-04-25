7f7aca4ad7ddd989a205e3644c940ae10688a2c1
Minsoo Kang <alstn38@naver.com>
2025-04-25 14:55:59 +0900

    feat: DownloadState Model 정의

---
+++ AppStalk/AppStalk/Domain/Entity/DownloadState.swift
@@ -0,0 +1,16 @@
+//
+//  DownloadState.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/25/25.
+//
+
+import Foundation
+
+enum DownloadState: String {
+    case ready
+    case downloading
+    case paused
+    case completed
+    case reinstall
+}
