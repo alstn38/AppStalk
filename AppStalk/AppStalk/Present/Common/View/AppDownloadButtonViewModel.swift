8249d4db79327aa980177df40904a32916a96baf
Minsoo Kang <alstn38@naver.com>
2025-04-27 02:09:06 +0900

    feat: AppDownloadButtonViewModel 정의 및 기능 구현

---
+++ AppStalk/AppStalk/Present/Common/View/AppDownloadButtonViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  AppDownloadButtonViewModel.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/26/25.
+//
+
+import Foundation
+import Combine
+
+final class AppDownloadButtonViewModel: ObservableObject {
+    
+    private var app: AppInfoEntity?
+    private let appSearchRepository: AppSearchRepository
+    
+    init(appSearchRepository: AppSearchRepository = DIContainer.shared.resolve(AppSearchRepository.self)) {
+        self.appSearchRepository = appSearchRepository
+    }
+    
+    func setupApp(_ app: AppInfoEntity) {
+        self.app = app
+    }
+    
+    func handleButtonTap() {
+        guard let app = app else { return }
+        
+        switch app.downloadState {
+        case .ready, .reinstall:
+            let downloadInfoDTO = AppDownloadInfoDTO()
+            downloadInfoDTO.appId = app.id
+            downloadInfoDTO.appName = app.trackName
+            downloadInfoDTO.iconURL = app.artworkUrl512
+            downloadInfoDTO.downloadState = DownloadState.downloading.rawValue
+            downloadInfoDTO.startTime = Date()
+            downloadInfoDTO.remainingSeconds = 30.0
+            
+            Task {
+                // 로컬 저장소에 다운로드 정보 저장
+                try? await DIContainer.shared.resolve(LocalStorageService.self).saveDownloadInfo(dto: downloadInfoDTO)
+                
+                // 다운로드 시작
+                await appSearchRepository.resumeDownload(appId: app.id)
+            }
+            
+        case .downloading:
+            // 다운로드 일시정지
+            Task {
+                await appSearchRepository.pauseDownload(appId: app.id)
+            }
+            
+        case .paused:
+            // 다운로드 재개
+            Task {
+                await appSearchRepository.resumeDownload(appId: app.id)
+            }
+            
+        case .completed:
+            break
+        }
+    }
+}
