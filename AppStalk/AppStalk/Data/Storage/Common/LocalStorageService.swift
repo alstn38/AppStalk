5d9604f1a533eb20f86e4915f75ec9911c2e0425
Minsoo Kang <alstn38@naver.com>
2025-04-26 17:21:33 +0900

    feat: LocalStorageService My App 기능 추가

--- AppStalk/AppStalk/Data/Storage/Common/LocalStorageService.swift
+++ AppStalk/AppStalk/Data/Storage/Common/LocalStorageService.swift
@@ -10,6 +10,8 @@ import RealmSwift
 
 protocol LocalStorageService {
     func fetchDownloadInfo(appId: Int) async throws -> AppDownloadInfoEntity?
+    func fetchCompletedDownloadInfos() async throws -> [AppDownloadInfoEntity]
+    func deleteDownloadInfo(appId: Int) async throws
 }
 
 @MainActor
@@ -33,5 +35,34 @@ final class DefaultLocalStorageService: LocalStorageService {
             lastUpdated: object.lastUpdated,
             currentBackgroundDate: object.currentBackgroundDate
         )
+    }
+    
+    func fetchCompletedDownloadInfos() async throws -> [AppDownloadInfoEntity] {
+        let completedObjects = realm.objects(AppDownloadInfoDTO.self)
+            .where { $0.downloadState == DownloadState.completed.rawValue }
+
+        return completedObjects.map { object in
+            AppDownloadInfoEntity(
+                appId: object.appId,
+                appName: object.appName,
+                iconURL: object.iconURL,
+                downloadState: DownloadState(rawValue: object.downloadState) ?? .ready,
+                startTime: object.startTime,
+                pausedTime: object.pausedTime,
+                remainingSeconds: object.remainingSeconds,
+                lastUpdated: object.lastUpdated,
+                currentBackgroundDate: object.currentBackgroundDate
+            )
+        }
+    }
+    
+    func deleteDownloadInfo(appId: Int) async throws {
+        guard let object = realm.object(ofType: AppDownloadInfoDTO.self, forPrimaryKey: appId) else {
+            return
+        }
+        
+        try realm.write {
+            realm.delete(object)
+        }
     }
 }
