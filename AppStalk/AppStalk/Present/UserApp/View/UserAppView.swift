2f6fd85d0006a4fb7863c73e702a959d164e9324
Minsoo Kang <alstn38@naver.com>
2025-04-26 17:22:09 +0900

    feat: UserAppView UI 구현

---
+++ AppStalk/AppStalk/Present/UserApp/View/UserAppView.swift
@@ -0,0 +1,40 @@
+//
+//  UserAppView.swift
+//  AppStalk
+//
+//  Created by 강민수 on 4/24/25.
+//
+
+import SwiftUI
+
+struct UserAppView: View {
+    @State private var searchText = ""
+    @StateObject private var viewModel = UserAppViewModel()
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.output.filteredApps, id: \.appId) { app in
+                    UserAppRow(app: app)
+                }
+                .onDelete(perform: delete)
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 앱")
+            .searchable(text: $searchText)
+            .onChange(of: searchText) { oldValue, newValue in
+                viewModel.input.queryChanged.send(newValue)
+            }
+            .onAppear {
+                viewModel.input.onAppear.send(())
+            }
+        }
+    }
+
+    private func delete(at offsets: IndexSet) {
+        for index in offsets {
+            let app = viewModel.output.filteredApps[index]
+            viewModel.input.deleteApp.send(app)
+        }
+    }
+}
